40ee0c58e486de3c0aae16c899abb1d0c197e90e
M.J. Bagheri Nejad <mjbn1380@gmail.com>
2021-06-02 12:29:12 +0430

    Adding guii & ins Func

--- alit.py
+++ alit.py
@@ -29,75 +29,74 @@ Arch Linux Installation Tool Version {}""".format(version)
     @property
     def cmdi(self, e):
         # Update the system clock
-        # sys("timedatectl set-ntp true")
+        sys("timedatectl set-ntp true")
 
         # Check if its UEFI
         chuefi = run(["ls", "/sys/firmware/efi/efivars"],
                      stdout=PIPE).stdout.decode("UTF-8")
-        print(chuefi.find("No such file or directory"))
         if chuefi.find("No such file or directory") == -1:
             print("BIOS")
-            # # Listing The Devices
-            # sys("fdisk -l")
+            # Listing The Devices
+            sys("fdisk -l")
 
-            # # Choose The Disk
-            # iDevice = str(
-            #     input(
-            #         "Choose the disk that you want to install on it ( /dev/sda ) => "
-            #     )
-            # )
-            # if iDevice == "":
-            #     iDevice = "/dev/sda"
+            # Choose The Disk
+            iDevice = str(
+                input(
+                    "Choose the disk that you want to install on it ( /dev/sda ) => "
+                )
+            )
+            if iDevice == "":
+                iDevice = "/dev/sda"
 
-            # # Partition the disks
-            # sys("pacman -S cfdisk")
-            # sys("cfdisk {}".format(iDevice))
-            # sys("clear")
+            # Partition the disks
+            sys("pacman -S cfdisk")
+            sys("cfdisk {}".format(iDevice))
+            sys("clear")
         else:
             print("UEFI: Not Supported")
 
-        # # Mount the file systems
-        # sys("fdisk -l")
-        # rootdev = str(input("which one is the root partition? "))
-        # homedev = str(input(
-        #     "which one is the home partition? (if you dont have one press enter) "
-        # ))
-        # sys("mount {} /mnt".format(rootdev))
-        # if homedev != "":
-        #     sys("mount {} /mnt/home".format(homedev))
+        # Mount the file systems
+        sys("fdisk -l")
+        rootdev = str(input("which one is the root partition? "))
+        homedev = str(input(
+            "which one is the home partition? (if you dont have one press enter) "
+        ))
+        sys("mount {} /mnt".format(rootdev))
+        if homedev != "":
+            sys("mount {} /mnt/home".format(homedev))
 
-        # # Install essential packages
-        # sys("pacstrap /mnt base linux linux-firmware")
+        # Install essential packages
+        sys("pacstrap /mnt base linux linux-firmware")
 
-        # # Generate an fstab file
-        # sys("genfstab -L /mnt >> /mnt/etc/fstab")
+        # Generate an fstab file
+        sys("genfstab -L /mnt >> /mnt/etc/fstab")
 
-        # # Change root into the new system
-        # sys("arch-chroot /mnt")
+        # Change root into the new system
+        sys("arch-chroot /mnt")
 
-        # # Set the time zone
-        # #sys("")
-        # #sys("hwclock --systohc")
+        # Set the time zone
+        #sys("")
+        #sys("hwclock --systohc")
 
-        # # Localization
-        # sys("echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen && locale-gen")
-        # sys("touch /etc/locale.conf && echo 'LANG=en_US.UTF-8' > /etc/locale.conf")
+        # Localization
+        sys("echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen && locale-gen")
+        sys("touch /etc/locale.conf && echo 'LANG=en_US.UTF-8' > /etc/locale.conf")
 
-        # # Create the hostname file:
-        # hn = str(input("Please Enter Your Hostname: "))
-        # sys("echo '{}' > /etc/hostname".format(hn))
-        # sys("echo '127.0.0.1\tlocalhost\n::1\tlocalhost\n127.0.1.1\t{}' > /etc/hosts".format(hn))
-        # sys("pacman -S networkmanager")
+        # Create the hostname file:
+        hn = str(input("Please Enter Your Hostname: "))
+        sys("echo '{}' > /etc/hostname".format(hn))
+        sys("echo '127.0.0.1\tlocalhost\n::1\tlocalhost\n127.0.1.1\t{}' > /etc/hosts".format(hn))
+        sys("pacman -S networkmanager")
 
-        # # Creating a new initramfs
-        # sys("mkinitcpio -P")
+        # Creating a new initramfs
+        sys("mkinitcpio -P")
 
-        # # Set the root password
-        # print("----Please Choose The Root Password-----")
-        # sys("passwd")
+        # Set the root password
+        print("----Please Choose The Root Password-----")
+        sys("passwd")
 
-        # # Boot loader
-        # sys("pacman -S grub && grub-install {}".format(iDevice))
+        # Boot loader
+        sys("pacman -S grub && grub-install {}".format(iDevice))
 
         # Exiting from program
         if e:
@@ -109,6 +108,32 @@ Arch Linux Installation Tool Version {}""".format(version)
     def guii(self):
         exit()
         self.cmdi(False)
+        gpu = run(["lspci","-v","|","grep","-A1","-e","VGA","-e","3D"],
+        stdout=PIPE).stdout.decode("UTF-8")
+        if gpu.find("intel") == -1 | gpu.find("Intel") == -1:
+            gpud = "xf86-video-intel"
+        elif gpu.find("nvidia") == -1 | gpu.find("Nvidia") == -1:
+            gpud = "nvidia"
+        elif gpu.find("amd") == -1 | gpu.find("AMD") == -1:
+            gpud = "xf86-video-amdgpu"
+        sys("pacman -S xorg lightdm lightdm-gtk-greeter {} && systemctl enable lightdm".format(gpud))
+        appl = str(input("Please Enter Your App List if you have one (Default: ./qtileAL.txt): "))
+        if appl == "":
+            appl == "./qtileAL.txt"
+        self.ins(appl)
+        self.ex()
+    
+    @property
+    def ins(self, s):
+        s = str(s)
+        f = open(s, "r")
+        lins = f.readlines()
+        cmd = "pacman -S"
+        for lin in lins:
+            cmd = "{} {}".format(cmd, lin)
+        
+        f.close()
+        sys(cmd)
 
     @property
     def ex(self):
